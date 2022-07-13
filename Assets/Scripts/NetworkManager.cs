3fd74465adc5b2d1faed857361bd6817713160e6
Artun Cimensel <cimensel.a@hotmail.com>
2022-07-13 15:56:38 -0700

    Added Photon to project, added scripts to add network players to scene
    need to fix updating other player transforms

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start(){
+        ConnectToServer();
+    }
+
+    void ConnectToServer(){
+        Debug.Log("Trying to connect to server...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster(){
+        Debug.Log("Connected to server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 5;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom(){
+        Debug.Log("Joined a room.");
+        base.OnJoinedRoom();
+    }
+
+    public override void  OnPlayerEnteredRoom(Player newPlayer){
+        Debug.Log("A new player joined the room.");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
