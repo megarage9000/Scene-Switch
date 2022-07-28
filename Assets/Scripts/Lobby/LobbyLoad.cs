db84ead5b97848798c4775b24bb23c081465b1f4
John Ordoyo <gtom_1984@hotmail.ca>
2022-07-27 19:31:17 -0700

    Work on Lobby

---
+++ Assets/Scripts/Lobby/LobbyLoad.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyLoad : MonoBehaviour
+{
+    public void LoadScene() { 
+    }
+}
---
+++ Assets/Scripts/Lobby/RoomSettings.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RoomSettings : MonoBehaviour
+{
+    static int SceneEnvironment = 1;
+    static int RoomNumber = 1;
+
+    public void SetScene(Int32 sceneNum) {
+        SceneEnvironment = sceneNum;
+    }
+
+    public void SetRoomNumber(Int32 roomNum) {
+        RoomNumber = roomNum;
+    }
+}
